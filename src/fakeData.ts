224ad5dd1d2a90c4906ddd20e53e6d6d410c666f
Luís Clício <cliciosa14@gmail.com>
2021-01-12 22:51:13 -0300

    feat: create fake data to dashboard

---
+++ src/fakeData.ts
@@ -0,0 +1,78 @@
+export default {
+  profiles: [
+    {
+      socialMedia: 'facebook',
+      username: '@nathanf',
+      followers: 1987,
+      todayFollowers: 12,
+    },
+    {
+      socialMedia: 'twitter',
+      username: '@nathanf',
+      followers: 1440,
+      todayFollowers: 99,
+    },
+    {
+      socialMedia: 'instagram',
+      username: '@realnathanf',
+      followers: 11000,
+      todayFollowers: 1099,
+    },
+    {
+      socialMedia: 'youtube',
+      username: 'Nathan F.',
+      followers: 8239,
+      todayFollowers: -144,
+    },
+  ],
+  overviews: [
+    {
+      socialMedia: 'facebook',
+      cardTitle: 'Page Views',
+      amount: 87,
+      percentage: 3,
+    },
+    {
+      socialMedia: 'facebook',
+      cardTitle: 'Likes',
+      amount: 52,
+      percentage: -2,
+    },
+    {
+      socialMedia: 'instagram',
+      cardTitle: 'Likes',
+      amount: 5462,
+      percentage: 2257,
+    },
+    {
+      socialMedia: 'instagram',
+      cardTitle: 'Profile Views',
+      amount: 52000,
+      percentage: 1375,
+    },
+    {
+      socialMedia: 'twitter',
+      cardTitle: 'Retweets',
+      amount: 117,
+      percentage: 303,
+    },
+    {
+      socialMedia: 'twitter',
+      cardTitle: 'Likes',
+      amount: 507,
+      percentage: 553,
+    },
+    {
+      socialMedia: 'youtube',
+      cardTitle: 'Likes',
+      amount: 107,
+      percentage: -19,
+    },
+    {
+      socialMedia: 'youtube',
+      cardTitle: 'Total Views',
+      amount: 1407,
+      percentage: -12,
+    },
+  ],
+};

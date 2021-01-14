3599e2356ae8a128a52c64769e02b4e5f5ef777a
Luís Clício <cliciosa14@gmail.com>
2021-01-14 11:23:53 -0300

    feat: create profile card component

---
+++ src/components/ProfileCard.tsx
@@ -0,0 +1,28 @@
+import { ProfileProps } from '../@types';
+import SocialIcon from './SocialIcon';
+import StatisticView from './StatisticView';
+import CounterView from './CounterView';
+
+import '../styles/components/ProfileCard.css';
+
+export default function ProfileCard({ profile }: { profile: ProfileProps }) {
+  return (
+    <article className={`profile-card profile-card--${profile.socialMedia}`}>
+      <header className="profile-card__header">
+        <SocialIcon name={profile.socialMedia} />
+        <h4 className="profile-card__header__username">{profile.username}</h4>
+      </header>
+
+      <section className="profile-card__followers">
+        <h3 className="profile-card__followers__amount">
+          <CounterView value={profile.followers} />
+        </h3>
+        <p className="profile-card__followers__text">followers</p>
+      </section>
+
+      <footer className="profile-card__footer">
+        <StatisticView value={profile.todayFollowers} text="Today" />
+      </footer>
+    </article>
+  );
+}

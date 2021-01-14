f96d16218ee97e3a280e33be909aad45233571f3
Luís Clício <cliciosa14@gmail.com>
2021-01-14 11:21:38 -0300

    feat: create statistic view component

---
+++ src/components/StatisticView.tsx
@@ -0,0 +1,34 @@
+import upIcon from '../assets/icon-up.svg';
+import downIcon from '../assets/icon-down.svg';
+
+import '../styles/components/StatisticView.css';
+
+interface StatisticViewProps {
+  value: number;
+  text: string;
+}
+
+export default function StatisticView({ value, text }: StatisticViewProps) {
+  const isPositive = value > 0;
+
+  function absolute(number: number) {
+    return number < 0 ? -number : number;
+  }
+
+  return (
+    <div className="statistic-view">
+      <img
+        src={isPositive ? upIcon : downIcon}
+        alt="Statistic"
+        className="statistic-view__image"
+      />
+      <span
+        className={`statistic-view__value statistic-view__value--${
+          isPositive ? 'positive' : 'negative'
+        }`}
+      >
+        {`${absolute(value)} ${text}`}
+      </span>
+    </div>
+  );
+}

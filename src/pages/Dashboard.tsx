b6b78119f6e77f985a70393c1522426e533c4487
Luís Clício <cliciosa14@gmail.com>
2021-01-14 11:25:09 -0300

    feat: add profile card into dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,3 +1,32 @@
+import { useState } from 'react';
+
+import { DashboardProps } from '../@types';
+import ProfileCard from '../components/ProfileCard';
+import TopBar from '../components/TopBar';
+
+import fakeData from '../fakeData';
+
+import '../styles/pages/Dashboard.css';
+
 export default function Dashboard() {
-  return <h1>Social Media Dashboard</h1>;
+  const [data, setData] = useState<DashboardProps>(fakeData);
+
+  return (
+    <>
+      <TopBar profiles={data.profiles} />
+
+      <main className="dashboard-content">
+        <section className="dashboard-content__profiles">
+          {data.profiles.map((profile) => (
+            <ProfileCard
+              key={`${profile.socialMedia}-${profile.followers}`}
+              profile={profile}
+            />
+          ))}
+        </section>
+
+        <section></section>
+      </main>
+    </>
+  );
 }
